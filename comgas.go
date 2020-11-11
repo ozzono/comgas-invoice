5e833100a5921015be455ac4bdfc98e3e80556bb
Hugo Virgilio <huvirgilio@gmail.com>
2020-11-10 22:39:36 -0300

    Fixed changes to invoice page; added verification steps; splitted into minor methods; added complete flow test case

--- comgas.go
+++ comgas.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/chromedp/chromedp"
 	"github.com/knq/chromedp/kb"
+	"github.com/ozzono/normalize"
 )
 
 var (
@@ -35,6 +36,7 @@ type Invoice struct {
 type UserData struct {
 	CPF  string `json:"cpf"`
 	Code string `json:"code"`
+	Name string `json:"name"`
 }
 
 //InvoiceFlow crawls through the enel page
@@ -49,7 +51,7 @@ func (flow *Flow) InvoiceFlow() (Invoice, error) {
 		return Invoice{}, err
 	}
 
-	invoice, err := flow.invoiceData()
+	invoice, err := flow.invoiceFlow()
 	if err != nil {
 		log.Println(err)
 		return Invoice{}, err
@@ -58,48 +60,51 @@ func (flow *Flow) InvoiceFlow() (Invoice, error) {
 }
 
 //InvoiceFlow crawls through the comgas page
-func (flow *Flow) invoiceData() (Invoice, error) {
-	log.Println("Starting Invoice Flow")
+func (flow *Flow) invoiceFlow() (Invoice, error) {
+	log.Println("starting Invoice Flow")
 
-	var (
-		dueDate   = ""
-		barCode   = ""
-		value     = ""
-		status    = "unknown"
-		outerHTML = ""
-	)
+	output := ""
 
 	err := chromedp.Run(flow.c,
 		chromedp.WaitVisible("i.icon-feather-log-out"),
-
 		chromedp.Text(
-			`document.querySelector("#mainContent > div > div > div.row.hidden-xs > div > div > table > tbody > tr:nth-child(1) > td:nth-child(2)")`,
-			&dueDate,
+			`document.querySelector("body > div.container.loggedin-container > div.row.loggedin-row > div.flex-container > div:nth-child(1) > div:nth-child(2) > div > section > a > span")`,
+			&output,
 			chromedp.ByJSPath,
 		),
+	)
+	if err != nil {
+		return Invoice{}, err
+	}
+	if !strings.Contains(strings.ToLower(output), "sair") {
+		return Invoice{}, fmt.Errorf("unable to continue flow; the page has changed")
+	}
 
-		chromedp.Text(
-			`document.querySelector("#mainContent > div > div > div.row.hidden-xs > div > div > table > tbody > tr:nth-child(1) > td:nth-child(3)")`,
-			&value,
-			chromedp.ByJSPath,
-		),
-
+	err = chromedp.Run(flow.c,
 		chromedp.OuterHTML(
-			`document.querySelector("#mainContent > div > div > div.row.hidden-xs > div > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > i")`,
-			&outerHTML,
+			`document.querySelector("#mainContent > app-root > div > div > div > div.row.hidden-xs > div > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > i")`,
+			&output,
 			chromedp.ByJSPath,
 		),
+	)
+	if err != nil {
+		return Invoice{}, err
+	}
+	if !strings.Contains(strings.ToLower(output), "a vencer") {
+		return Invoice{}, fmt.Errorf("no pending invoices")
+	}
 
-		chromedp.Click(
-			`document.querySelector("#mainContent > div > div > div.row.hidden-xs > div > div > table > tbody > tr:nth-child(1) > td.ng-scope > span")`,
-			chromedp.NodeVisible,
-			chromedp.ByJSPath,
-		),
-		chromedp.WaitVisible("body > div.modal.fade.ng-isolate-scope.in > div > div > div.modal-body.ng-scope > div > div > div.panel.ng-scope > div > p:nth-child(1) > strong"),
+	invoice := Invoice{Status: "pending"}
 
+	err = chromedp.Run(flow.c,
 		chromedp.Text(
-			`document.querySelector("body > div.modal.fade.ng-isolate-scope.in > div > div > div.modal-body.ng-scope > div > div > div.panel.ng-scope > div > p:nth-child(2) > span")`,
-			&barCode,
+			`document.querySelector("#mainContent > app-root > div > div > div > div.row.hidden-xs > div > div > table > tbody > tr:nth-child(1) > td:nth-child(2)")`,
+			&invoice.DueDate,
+			chromedp.ByJSPath,
+		),
+		chromedp.Text(
+			`document.querySelector("#mainContent > app-root > div > div > div > div.row.hidden-xs > div > div > table > tbody > tr:nth-child(1) > td:nth-child(3)")`,
+			&invoice.Value,
 			chromedp.ByJSPath,
 		),
 	)
@@ -107,21 +112,54 @@ func (flow *Flow) invoiceData() (Invoice, error) {
 	if err != nil {
 		return Invoice{}, err
 	}
-
-	if strings.Contains(outerHTML, "A vencer") {
-		status = "pending"
+	if len(invoice.Value) == 0 {
+		return Invoice{}, fmt.Errorf("failed to fetch invoice value")
 	}
+	if len(invoice.DueDate) == 0 {
+		return Invoice{}, fmt.Errorf("failed to fetch invoice duedate")
+	}
+
+	err = chromedp.Run(flow.c,
+		chromedp.Click(`document.querySelector("#mainContent > app-root > div > div > div > div.row.hidden-xs > div > div > table > tbody > tr:nth-child(1) > td.ng-scope > span")`,
+			chromedp.NodeVisible,
+			chromedp.ByJSPath,
+		),
+		chromedp.WaitVisible("i.icon-fechar"),
+		chromedp.Text(
+			`document.querySelector("body > div.modal.fade.ng-isolate-scope.in > div > div > div.modal-body.ng-scope > div > div > div.panel.ng-scope > div > p:nth-child(1) > strong")`,
+			&output,
+			chromedp.ByJSPath,
+		),
+	)
+	if err != nil {
+		return Invoice{}, err
+	}
+	if !strings.Contains(strings.ToLower(normalize.Norm(output)), "numero do codigo de barra") {
+		return Invoice{}, fmt.Errorf("unable to continue flow; the page has changed")
+	}
+
+	err = chromedp.Run(flow.c,
+		chromedp.Text(
+			`document.querySelector("body > div.modal.fade.ng-isolate-scope.in > div > div > div.modal-body.ng-scope > div > div > div.panel.ng-scope > div > p:nth-child(2) > span")`,
+			&invoice.BarCode,
+			chromedp.ByJSPath,
+		),
+	)
+	if err != nil {
+		return Invoice{}, err
+	}
+	if len(invoice.BarCode) == 0 {
+		return Invoice{}, fmt.Errorf("failed to fetch invoice barcode")
+	}
+
 	log.Println("Successfully finished Invoice flow")
-	return Invoice{
-		Value:   strings.Replace(strings.TrimPrefix(value, "R$ "), ",", ".", -1),
-		BarCode: strings.Replace(barCode, " ", "", -1),
-		DueDate: dueDate,
-		Status:  status,
-	}, nil
+	invoice.Value = strings.TrimPrefix(invoice.Value, "R$ ")
+	invoice.BarCode = strings.Replace(invoice.BarCode, " ", "", -1)
+	return invoice, nil
 }
 
 func (flow *Flow) login() error {
-	log.Println("Starting login flow")
+	log.Println("starting login flow")
 	output := ""
 	err := chromedp.Run(flow.c,
 		chromedp.Navigate("https://virtual.comgas.com.br/#/comgasvirtual/historicoFaturas"),
@@ -149,12 +187,23 @@ func (flow *Flow) login() error {
 		chromedp.SendKeys(`document.querySelector("#modalTxtCodigoUsuario")`, kb.End+flow.User.Code, chromedp.ByJSPath),
 
 		chromedp.Click(`document.querySelector("#btnEnviar > span")`, chromedp.NodeVisible, chromedp.ByJSPath),
-		chromedp.Sleep(5*time.Second),
+		chromedp.WaitVisible("h4.ng-binding"),
+		chromedp.Text(
+			`document.querySelector("body > div.container.loggedin-container > div.row.loggedin-row > div.flex-container > div:nth-child(1) > div:nth-child(2) > div > section > a > span")`,
+			&output,
+			chromedp.ByJSPath,
+		),
 	)
 	if err != nil {
 		return err
 	}
-	log.Println("Successfully finished login flow")
+	if len(output) == 0 {
+		return fmt.Errorf("unable to continue flow; the page has changed")
+	}
+	if !strings.Contains(strings.ToLower(normalize.Norm(output)), "sair") {
+		return fmt.Errorf("failed to log in; user name does no match: %s!=%s", output, flow.User.Name)
+	}
+	log.Println("successfully finished login flow")
 	return nil
 }
 
---
+++ comgas_test.go
@@ -0,0 +1,34 @@
+package comgas
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	f := NewFlow(false)
+	user, err := readFile("config_test.json")
+	if err != nil {
+		t.Logf("readFile err: %v", err)
+	}
+	f.User = user
+	invoice, err := f.InvoiceFlow()
+	if err != nil {
+		t.Logf("f.InvoiceFlow err: %v", err)
+	}
+	t.Logf("invoice: %#v", invoice)
+}
+
+func readFile(filename string) (UserData, error) {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return UserData{}, err
+	}
+	output := UserData{}
+	err = json.Unmarshal(file, &output)
+	if err != nil {
+		return UserData{}, err
+	}
+	return output, nil
+}
